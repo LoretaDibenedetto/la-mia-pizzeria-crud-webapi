8c2644dda5b4ec9a04a8ea477233501940ae9978
LoretaDibenedetto <dibenedettoloreta@gmail.com>
2023-05-29 14:15:18 +0200

    add PizzaApiController

---
+++ LaMiaPizzeria/Controllers/API/PizzaAPIController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LaMiaPizzeria.Controllers.API
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class PizzaAPIController : ControllerBase
+    {
+
+    }
+}
