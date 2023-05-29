06517e6dcb7edd3dc89e0701433e247e944a4168
LoretaDibenedetto <dibenedettoloreta@gmail.com>
2023-05-29 14:25:51 +0200

    add getPizzas method

--- LaMiaPizzeria/Controllers/API/PizzaAPIController.cs
+++ LaMiaPizzeria/Controllers/API/PizzaAPIController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using LaMiaPizzeria.Database;
+using LaMiaPizzeria.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LaMiaPizzeria.Controllers.API
@@ -7,6 +9,21 @@ namespace LaMiaPizzeria.Controllers.API
     [ApiController]
     public class PizzaAPIController : ControllerBase
     {
+
+        [HttpGet]
+        public IActionResult GetPizzas()
+        {
+
+            using(PizzaContext db = new PizzaContext())
+            {
+                List<Pizza> pizzas = db.Pizzas.ToList();
+                return Ok(pizzas);
+            }
+
+        }
+
+        
+
 
     }
 }
