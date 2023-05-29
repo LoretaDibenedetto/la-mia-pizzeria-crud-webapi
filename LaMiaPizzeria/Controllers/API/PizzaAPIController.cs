92b25fd7cedd48c93db8b49c0f053ce3ce082ce1
LoretaDibenedetto <dibenedettoloreta@gmail.com>
2023-05-29 15:08:21 +0200

    bonus

--- LaMiaPizzeria/Controllers/API/PizzaAPIController.cs
+++ LaMiaPizzeria/Controllers/API/PizzaAPIController.cs
@@ -57,10 +57,70 @@ namespace LaMiaPizzeria.Controllers.API
                  return NotFound();
 
                 }
+
         
-        
-        
-        
+        }
+
+
+        [HttpPost]
+        public IActionResult Create([FromBody] Pizza pizza)
+        {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+            else { 
+
+                using(var db = new PizzaContext())
+                {
+                    db.Pizzas.Add(pizza);
+                    db.SaveChanges();
+
+                    return Ok();
+                }
+           }
+        }
+
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] Pizza data)
+        {
+            using (PizzaContext db = new PizzaContext())
+            {
+               
+              Pizza? pizzaIdToEdit = db.Pizzas.Where(pizza => pizza.Id == id).FirstOrDefault();
+
+                if (pizzaIdToEdit != null)
+                {
+                    
+                    return Ok();
+                }
+                else
+                {
+                    
+                    return NotFound();
+                }
+            }
+        }
+
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            using (PizzaContext context = new PizzaContext())
+            {
+                Pizza? profileToDelete = context.Pizzas.Where(profile => profile.Id == id).FirstOrDefault();
+                if (profileToDelete != null)
+                {
+                    context.Pizzas.Remove(profileToDelete);
+                    context.SaveChanges();
+                    return Ok();
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
         }
 
     }
